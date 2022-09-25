41ab0323b0020220732ac1573f4fe6bbf059954a
Gracechung-sw <hjngy0511@gmail.com>
2022-09-25 22:23:16 +0900

    Fix bad function using generic constraints

--- generics/constraints.ts
+++ generics/constraints.ts
@@ -16,7 +16,9 @@ class PartTimeEmployee implements Employee {
   workPartTime() {}
 }
 
-function pay(employee: Employee): Employee {
+// generic이긴 한데, 조건을 걸어 줄 수 있다.
+// generic type인데, 이건 Employee를 확장한 타입들만 가능해
+function pay<T extends Employee>(employee: T): T {
   employee.pay();
   return employee;
 }
@@ -28,8 +30,5 @@ grace.workFullTime();
 bob.workPartTime();
 
 const graceAfterPay = pay(grace);
-graceAfterPay.workFullTime(); // Property 'workFullTime' does not exist on type 'Employee'. 라는 에러 발생 왜?
-// pay라는 함 수 사용하고 나서는 리턴타입이 Employee이라서 FullTimeEmployee이 가진 class 정보가 사라졌기 때문.
-// 이를 해결하기 위해 const graceAfterPay = pay(grace) as FullTimeEmployee 라고 할 수도 있지만 가능하면 as는 사용하지 않는 것이 좋다.
-// 이런 경우 generic을 사용할 수 있다.
+graceAfterPay.workFullTime();
 const bobAfterPay = pay(bob);
