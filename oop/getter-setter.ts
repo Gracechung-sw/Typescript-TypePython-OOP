ac867c7003b1708aff32f12daddf2194d70a347c
Gracechung-sw <hjngy0511@gmail.com>
2022-09-25 10:40:45 +0900

    Fix to use getter

--- oop/getter-setter.ts
+++ oop/getter-setter.ts
@@ -1,13 +1,16 @@
 {
+  // 이런 경우 getter, setter 사용이 필요!
   class User {
     firstName: string;
     lastName: string;
-    fullName: string;
 
     constructor(firstName: string, lastName: string) {
       this.firstName = firstName;
       this.lastName = lastName;
-      this.fullName = `${firstName} ${lastName}`;
+    }
+
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
     }
   }
 
@@ -15,6 +18,5 @@
   console.log(user.fullName); // Steve Jobs
 
   user.firstName = 'Grace';
-  console.log(user.fullName); // Steve Jobs  // 왜 Grace Jobs가 아닐까?
-  // instance가 생성될 때 constructor가 한 번 실행 된 이후 firstName이 수정되어도 fullName 생성은 다시 실행되지 않기 때문.
+  console.log(user.fullName); // Grace Jobs
 }
