e9ad1c31b9eb0c2dfe55fc9e9375949f4f20f86a
Gracechung-sw <hjngy0511@gmail.com>
2022-09-25 10:44:49 +0900

    Make simple of constructor member variables

--- oop/getter-setter.ts
+++ oop/getter-setter.ts
@@ -1,13 +1,8 @@
 {
   // 이런 경우 getter, setter 사용이 필요!
   class User {
-    private firstName: string;
-    private lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-    }
+    // 생성자(constructor)의 인자에 접근제한자를 설정해주면 바로 member 변수로 생성이 된다.
+    constructor(private firstName: string, private lastName: string) {}
 
     get fullName(): string {
       return `${this.firstName} ${this.lastName}`;
@@ -16,4 +11,6 @@
 
   const user = new User('Steve', 'Jobs');
   console.log(user.fullName); // Steve Jobs
+
+  user.firstName = 'dsfa'; // Property 'firstName' is private and only accessible within class 'User'.ts(2341)
 }
