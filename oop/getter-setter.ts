18ce0472e5e1fa8e9c0a5dd7b68bcb744628d0c8
Gracechung-sw <hjngy0511@gmail.com>
2022-09-25 10:41:54 +0900

    Fix firstName, lastName to private

--- oop/getter-setter.ts
+++ oop/getter-setter.ts
@@ -1,8 +1,8 @@
 {
   // 이런 경우 getter, setter 사용이 필요!
   class User {
-    firstName: string;
-    lastName: string;
+    private firstName: string;
+    private lastName: string;
 
     constructor(firstName: string, lastName: string) {
       this.firstName = firstName;
@@ -16,7 +16,4 @@
 
   const user = new User('Steve', 'Jobs');
   console.log(user.fullName); // Steve Jobs
-
-  user.firstName = 'Grace';
-  console.log(user.fullName); // Grace Jobs
 }
