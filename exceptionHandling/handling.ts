cbf10eb4a15d0babe4599d68bb3a71193f53f416
Gracechung-sw <hjngy0511@gmail.com>
2022-10-01 11:28:48 +0900

    Fix how to handle exception

--- exceptionHandling/handling.ts
+++ exceptionHandling/handling.ts
@@ -7,15 +7,7 @@ class NetworkClient {
 class UserService {
   constructor(private client: NetworkClient) {}
   login() {
-    try {
-      this.client.tryConnect();
-      //login...
-    } catch (error) {
-      console.log('catched!');
-    } // 이렇게 수정해주면, error를 catch해서 application은 죽지 않겠지만, 그래서 뭐? 이 에러를 어떻게 핸들링했는데?
-    // 사실 아무것도 처리하지 않은 것이고, 심지어 application은 에러가 발생했는지 조차 알 수 없다.
-    // Point!: 에러가 발생했을 때, 우아/정확하게 handling할 수 없는 에러라면 catch하지 않는 편이 더 좋다.
-    // 그리고 이 에러는 처리할 수 있는 곳에서 catch하고 처리하는 것이 더 좋다.
+    this.client.tryConnect();
   }
 }
 
@@ -32,7 +24,11 @@ Error: no network!
 class App {
   constructor(private userService: UserService) {}
   run() {
-    this.userService.login();
+    try {
+      this.userService.login();
+    } catch (error) {
+      // do sth...
+    }
   }
 }
 
