e299fc0e859908476fab49d2d474900c2d81377e
Gracechung-sw <hjngy0511@gmail.com>
2022-11-13 01:35:42 +0900

    Add sample code of python optional in class type

--- type-python-oop/basicType/optional-type.py
+++ type-python-oop/basicType/optional-type.py
@@ -8,3 +8,15 @@ xxx = None
 # 위와 아래가 같은 의미이다. 
 xxx: Optional[str] = 'hello'
 xxx = None
+
+
+# class안에서 자기 자신의 타입을 타이핑 할 때, "" 로 감싸준다. 
+# 링크드리스트의 Node를 class로 구현할 띠를 생각해보면, 
+class Node:
+  def __init__(self, data: int, node: Optional["Node"]):
+    self.data = data
+    self.node = node
+
+header = Node(1, None)
+
+node = Node(2, header)
